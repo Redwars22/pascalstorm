5550126cc8abe48b095618dff850db6d732d3fea
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-10-09 18:30:11 -0300

    adicionando funções de copiar, colar, selecionar tudo, salvar, abrir e criar novo

--- main.pas
+++ main.pas
@@ -13,27 +13,55 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    About: TMenuItem;
+    CopyProgr: TMenuItem;
+    ExitProgr: TMenuItem;
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
     MenuItem4: TMenuItem;
     MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    Open: TMenuItem;
     OpenDialog1: TOpenDialog;
+    PasteProgr: TMenuItem;
+    Save: TMenuItem;
     SaveDialog1: TSaveDialog;
+    SelectAllProgr: TMenuItem;
+    Separator1: TMenuItem;
+    Separator2: TMenuItem;
+    Separator3: TMenuItem;
     SynEdit1: TSynEdit;
     SynPasSyn1: TSynPasSyn;
-    procedure MenuItem2Click(Sender: TObject);
-    procedure MenuItem3Click(Sender: TObject);
+    procedure AboutClick(Sender: TObject);
+    procedure CopyProgrClick(Sender: TObject);
+    procedure MenuItem4Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
+    procedure OpenClick(Sender: TObject);
+    procedure PasteProgrClick(Sender: TObject);
+    procedure SaveClick(Sender: TObject);
+    procedure SelectAllProgrClick(Sender: TObject);
   private
 
   public
 
   end;
 
+const
+  BoilerplateCode =
+    'program HelloWorld;' + sLineBreak +
+    'begin' + sLineBreak +
+    '  writeln(''Hello, World!'');' + sLineBreak +
+    'end.';
+
 var
   Form1: TForm1;
+  FileContent: TStringList;
+  searchText: string;
+  startPos: LongInt;
+  FoundPos: Integer;
 
 implementation
 
@@ -41,26 +69,68 @@ implementation
 
 { TForm1 }
 
+procedure TForm1.SaveClick(Sender: TObject);
+begin
+     if SaveDialog1.Execute then
+        begin
+             SynEdit1.Lines.SaveToFile( SaveDialog1.Filename );
+             Form1.Caption := SaveDialog1.Filename + ' - Andrew''s PascalStorm 2023';
+        end;
+end;
+
+procedure TForm1.SelectAllProgrClick(Sender: TObject);
+begin
+  SynEdit1.SelectAll;
+end;
+
+procedure TForm1.AboutClick(Sender: TObject);
+begin
+    MessageDlg('Andrew''s PascalStorm' + LineEnding +
+             'Version: 2023' + LineEnding +
+             'Copyright © 2023 AndrewNation Software, Inc.' + LineEnding +
+             'Website: https://https://andrewnationdev.vercel.app' + LineEnding +
+             'Email: andrewnationdev@gmail.com',
+             mtInformation, [mbOK], 0);
+end;
+
+procedure TForm1.CopyProgrClick(Sender: TObject);
+begin
+  SynEdit1.CopyToClipboard;
+end;
+
+procedure TForm1.MenuItem4Click(Sender: TObject);
+begin
+  SynEdit1.Clear;
+  SynEdit1.Text := BoilerplateCode;
+end;
+
 procedure TForm1.MenuItem5Click(Sender: TObject);
 begin
 
 end;
 
-procedure TForm1.MenuItem3Click(Sender: TObject);
+procedure TForm1.OpenClick(Sender: TObject);
 begin
-   if SaveDialog1.Execute then
-    SynEdit1.Lines.SaveToFile( SaveDialog1.Filename );
+    if fileExists(OpenDialog1.Filename) then
+    begin
+         FileContent := TStringList.Create;
+         try
+           FileContent.LoadFromFile(OpenDialog1.Filename);
+           SynEdit1.Lines.Assign(FileContent);
+         finally
+           FileContent.Free;
+           Form1.Caption := OpenDialog1.Filename + ' - Andrew''s PascalStorm 2023';
+         end;
+
+         Exit;
+    end;
+
+    ShowMessage('No file selected');
 end;
 
-procedure TForm1.MenuItem2Click(Sender: TObject);
+procedure TForm1.PasteProgrClick(Sender: TObject);
 begin
-   if OpenDialog1.Execute then
-   begin
-     if fileExists(OpenDialog1.Filename) then
-       ShowMessage(OpenDialog1.Filename);
-   end
-   else
-       ShowMessage('No file selected');
+  SynEdit1.PasteFromClipboard;
 end;
 
 end.
