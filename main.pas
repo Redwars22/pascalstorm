219cc1c3d848e767f697ef1643913e96f6555059
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-10-10 20:30:08 -0300

    autosave e compilar a partir do PascalStorm

--- main.pas
+++ main.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, SynEdit,
-  SynHighlighterPas;
+  SynHighlighterPas, Process;
 
 type
 
@@ -37,12 +37,16 @@ type
     SynPasSyn1: TSynPasSyn;
     procedure AboutClick(Sender: TObject);
     procedure CopyProgrClick(Sender: TObject);
+    procedure ExitProgrClick(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
     procedure MenuItem5Click(Sender: TObject);
+    procedure MenuItem8Click(Sender: TObject);
     procedure OpenClick(Sender: TObject);
     procedure PasteProgrClick(Sender: TObject);
     procedure SaveClick(Sender: TObject);
     procedure SelectAllProgrClick(Sender: TObject);
+    procedure SynEdit1Change(Sender: TObject);
   private
 
   public
@@ -62,6 +66,9 @@ var
   searchText: string;
   startPos: LongInt;
   FoundPos: Integer;
+  CompilerProcess: TProcess;
+  filename: string;
+  CompileSuccess: Boolean;
 
 implementation
 
@@ -75,12 +82,24 @@ begin
         begin
              SynEdit1.Lines.SaveToFile( SaveDialog1.Filename );
              Form1.Caption := SaveDialog1.Filename + ' - Andrew''s PascalStorm 2023';
+             filename := SaveDialog1.Filename;
         end;
 end;
 
 procedure TForm1.SelectAllProgrClick(Sender: TObject);
 begin
   SynEdit1.SelectAll;
+end;
+
+procedure TForm1.SynEdit1Change(Sender: TObject);
+begin
+  If MenuItem2.Checked then
+  begin
+       If filename <> '' then
+       begin
+          SynEdit1.Lines.SaveToFile( filename );
+       end;
+  end;
 end;
 
 procedure TForm1.AboutClick(Sender: TObject);
@@ -98,6 +117,22 @@ begin
   SynEdit1.CopyToClipboard;
 end;
 
+procedure TForm1.ExitProgrClick(Sender: TObject);
+begin
+  Halt(0);
+end;
+
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+  If not MenuItem2.Checked then
+  begin
+      MenuItem2.Checked := true;
+      Exit;
+  end;
+
+  MenuItem2.Checked := false;
+end;
+
 procedure TForm1.MenuItem4Click(Sender: TObject);
 begin
   SynEdit1.Clear;
@@ -109,8 +144,42 @@ begin
 
 end;
 
+procedure TForm1.MenuItem8Click(Sender: TObject);
+begin
+    CompilerProcess := TProcess.Create(nil);
+  try
+    CompilerProcess.Executable := 'fpc'; // Path to the Free Pascal Compiler executable
+    CompilerProcess.Parameters.Add(filename); // Replace with your source file path
+    CompilerProcess.Options := [poWaitOnExit];
+    CompilerProcess.Execute;
+
+    CompileSuccess := CompilerProcess.ExitStatus = 0; // Check if the compilation was successful
+
+    if CompileSuccess then
+    begin
+      MessageDlg('Project compiled successfully', mtInformation, [mbOK], 0);
+
+      // Optionally, you can execute the compiled binary here
+      // For example, if your Pascal program creates an executable file named "myprogram.exe":
+      // CompilerProcess.Executable := 'myprogram.exe';
+      // CompilerProcess.Parameters.Clear;
+      // CompilerProcess.Execute;
+    end
+    else
+    begin
+      MessageDlg('Compilation failed', mtError, [mbOK], 0);
+    end;
+  except
+    on E: Exception do
+    begin
+      MessageDlg(E.Message, mtError, [mbOK], 0);
+    end;
+  end;
+end;
+
 procedure TForm1.OpenClick(Sender: TObject);
 begin
+  if OpenDIalog1.Execute then
     if fileExists(OpenDialog1.Filename) then
     begin
          FileContent := TStringList.Create;
@@ -120,6 +189,7 @@ begin
          finally
            FileContent.Free;
            Form1.Caption := OpenDialog1.Filename + ' - Andrew''s PascalStorm 2023';
+           filename := OpenDialog1.Filename;
          end;
 
          Exit;
--- pascalstorm.lpr
+++ pascalstorm.lpr
@@ -17,6 +17,7 @@ uses
 
 begin
   RequireDerivedFormResource:=True;
+  Application.Title:='PascalStorm 2023';
   Application.Scaled:=True;
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
